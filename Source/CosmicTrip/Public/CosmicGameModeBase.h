e99809b3146d787aec262d471c7b3df3d9bf6444
jang <jangdave@naver.com>
2023-02-28 15:23:09 +0900

    Initial commit

---
+++ Source/CosmicTrip/Public/CosmicGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CosmicGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COSMICTRIP_API ACosmicGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
