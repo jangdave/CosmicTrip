cf491f77545c438579e0578dd718733cd072b527
yeni <yeniyeni0224@gmail.com>
2023-03-02 18:44:09 +0900

    230302 Enemy Movement
    Set Enemy FSM structure
    Enemy Follows Player
    Create Animation Instance class

---
+++ Source/CosmicTrip/Public/CloseAttackEnemyAnim.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "CloseAttackEnemyAnim.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COSMICTRIP_API UCloseAttackEnemyAnim : public UAnimInstance
+{
+	GENERATED_BODY()
+
+
+	
+};
