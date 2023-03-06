0ea1af4b4a05af66c2aff6f22984d2a313a37042
Jellyforest <jaewon0618@nate.com>
2023-03-06 20:18:48 +0900

    3/6 20:20 총알구현
     20:20 총알구현

--- Source/CosmicTrip/Public/BulletActor.h
+++ Source/CosmicTrip/Public/BulletActor.h
@@ -35,4 +35,7 @@ public:
 
 	UFUNCTION() //언리얼에서 인식할수있는 형식
 	void OnDie();
+
+	UFUNCTION()
+	void BulletPower(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 };
